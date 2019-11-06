07d4699efe8bf2c35c6612dc155135fa0fa868fd
Niels Kersic <apps@niels.codes>
2019-11-06 22:58:35 +0100

    Basic material layout with Flutter
    Added basic layout with a bottom navigation bar and a floating action button

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          brightness: Brightness.light, bottomAppBarColor: Colors.blue),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        bottomAppBarColor: Colors.black,
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Hello"),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {},
+            backgroundColor: Colors.red,
+          ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          bottomNavigationBar: BottomAppBar(
+            shape: CircularNotchedRectangle(),
+            notchMargin: 4.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.whatshot),
+                  onPressed: () {},
+                  iconSize: 30,
+                ),
+                IconButton(
+                  icon: Icon(Icons.person),
+                  onPressed: () {},
+                  iconSize: 30,
+                )
+              ],
+            ),
+          ),
+          body: Text("Foo bar")),
+    );
+  }
+}
